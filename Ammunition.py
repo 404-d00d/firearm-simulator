59e5386a4ec27e44de678ed7112cf18e3cc8231b
404-d00d <67910702+404-d00d@users.noreply.github.com>
2023-11-15 10:19:26 -0500

    Add files via upload

--- Ammunition.py
+++ Ammunition.py
@@ -6,6 +6,7 @@ gauge12_2and3quarters = [["Winchester Super-X|12 Gauge|00 Buckshot", 5, "KATHOOO
                          ["Federal Power-Shok|12 Gauge|000 Buckshot", 250, "KATHOOOOOOOM", 70, 1325, 69.85, 8, 0.001],
                          ["Sellier & Bellot|12 Gauge|#1 Buckshot", 250, "KATHOOOOOOOOM", 40.02, 1200, 69.85, 12, 0.001]]
 
+#5.56 guns can chamber .223 Remington and vise versa (the latter is not exactly recommended)
 remington223 = [["Precision Made Cartridges|.223 Remington|FMJ-BT", 20, "KTHRAAAAAAAK", 55, 2900, 57, 1, 0.001],
                 ["Black Hills Ammunition|.223 Remington|HP", 20, "KTHRAAAAAAAAK", 75, 2750, 57, 1, 0.001]]
 
@@ -16,6 +17,7 @@ bore410_2and1half = [["Fiocchi Target|.410 Bore|#8 Birdshot", 25, "KATHRAAAAK", 
 # 22lr firearms can also fire 22short, 22long, 22bb, and 22cb
 lr22 = [["CCI Mini Mag|.22 LR|Round Nose", 100, "KRAAAAAAAK", 40, 1235, 25.4, 1, 0.01]]
 
+#can load 38 special into 357 magnum guns, CAN NOT do the reverse
 magnum357 = [["Black Hills|.357 Magnum|Lead Round Nose", 50, "KTHOOOOOM", 158, 800, 40, 1, 0.001]]
 
 special38 = [["Remington Performance|.38 Special|Lead Semi Wadcutter", 50, "KTHOOOOOM", 158, 755, 39.4, 1, 0.001]]
